4cc22421e3decbe7afc735be5425eb3bbdd8813d
allezz <aerbay@gmail.com>
2020-12-18 18:40:37 +0100

    Change folder structure

---
+++ 2020/day2/day2.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const text = fs.readFileSync("./data.txt").toString("utf-8");
+const data = text.split("\n");
+
+let checkPass = (arr) => {
+  let valid = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let numberRule = arr[i].match(/\d+-\d+/)[0].split("-");
+    let letterRule = arr[i].match(/\w:/)[0].split("")[0];
+    let password = arr[i].match(/\w*$/)[0].split("");
+    let occurence = password.filter((e) => e === letterRule).length;
+
+    if (
+      occurence >= parseInt(numberRule[0]) &&
+      occurence <= parseInt(numberRule[1])
+    ) {
+      valid++;
+    }
+  }
+  return console.log(valid);
+};
+
+// checkPass(data);
+
+let checkPassPart2 = (arr) => {
+  let valid = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let numberRule = arr[i].match(/\d+-\d+/)[0].split("-");
+    let letterRule = arr[i].match(/\w:/)[0].split("")[0];
+    let password = arr[i].match(/\w*$/)[0].split("");
+
+    if (
+      (password[parseInt(numberRule[0]) - 1] === letterRule &&
+        password[parseInt(numberRule[1]) - 1] !== letterRule) ||
+      (password[parseInt(numberRule[0]) - 1] !== letterRule &&
+        password[parseInt(numberRule[1]) - 1] === letterRule)
+    ) {
+      valid++;
+    }
+  }
+  return console.log(valid);
+};
+
+checkPassPart2(data);
