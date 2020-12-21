e41be6ca5681693d8eb6fd97ccd0fa4bdfad6b51
allezz <aerbay@gmail.com>
2020-12-22 00:07:50 +0100

    Add day1 part 1 solution

--- 2019/day1/day1.js
+++ 2019/day1/day1.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 const text = fs.readFileSync("./data.txt").toString("utf-8");
 const data = text.split("\n");
-
 console.log(data);
+let sum = 0;
+for (let i = 0; i < data.length; i++) {
+  sum = sum + (Math.floor(parseInt(data[i]) / 3) - 2);
+}
+console.log(sum);
