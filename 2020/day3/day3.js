8c2894e8479cc1f87c316f8cc27d1434280c4914
allezz <aerbay@gmail.com>
2020-12-21 15:41:37 +0100

    Add day3

--- 2020/day3/day3.js
+++ 2020/day3/day3.js
@@ -2,4 +2,21 @@ const fs = require("fs");
 const text = fs.readFileSync("./data.txt").toString("utf-8");
 const data = text.split("\n");
 const matrice = data.map((e) => e.split(""));
-console.log(matrice);
+
+let newM = [...matrice];
+for (let j = 0; j < matrice.length; j++) {
+  for (let i = 0; i < matrice.length; i++) {
+    newM[i] = newM[i].concat(matrice[i]);
+  }
+}
+const stepDown = (arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][i * 3] === "#") {
+      count++;
+    }
+  }
+  return console.log(count);
+};
+
+stepDown(newM);
