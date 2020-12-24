dac8a7d45988385484801c2fa4a18b067e6087fa
allezz <aerbay@gmail.com>
2020-12-24 06:13:58 +0100

    Add solution day day4 part 2

--- 2019/day4/day4.js
+++ 2019/day4/day4.js
@@ -1,8 +1,9 @@
 let countValidPasswords = () => {
   let validCount = 0;
-  for (let i = 359282; i < 820401; i++) {
+  for (let i = 359282; i < 820402; i++) {
     let current = Array.from(String(i), Number);
     if (checkAdjacentRule2(current) && checkIncreaseRule(current)) {
+      console.log(current);
       validCount++;
     }
   }
@@ -22,23 +23,27 @@ let checkAdjacentRule = (arr) => {
 
 let checkAdjacentRule2 = (arr) => {
   let countObj = {};
-  for (let i = 0; i < arr.length - 1; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] == arr[i + 1]) {
       if (!countObj[arr[i]]) {
-        countObj[arr[i]] = 2;
+        countObj[arr[i]] = [i, i + 1];
       } else {
-        countObj[arr[i]]++;
+        if (arr[i - 1] !== arr[i]) {
+          countObj[arr[i]].push(i, i + 1);
+        } else {
+          countObj[arr[i]].push(i + 1);
+        }
       }
     }
   }
   for (let n in countObj) {
-    if (countObj[n] % 2 == 0) {
+    if (countObj[n].length != 2) {
       continue;
-    } else if (countObj[n] % 2 !== 0) {
-      return false;
+    } else if (countObj[n].length == 2) {
+      return true;
     }
   }
-  return true;
+  return false;
 };
 
 let checkIncreaseRule = (arr) => {
