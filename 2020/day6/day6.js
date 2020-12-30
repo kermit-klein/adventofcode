830a4f9bbe6394599481fff25655e897cda3a258
allezz <aerbay@gmail.com>
2020-12-30 04:28:48 +0100

    Add solution for day 6 part 2

--- 2020/day6/day6.js
+++ 2020/day6/day6.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const text = fs.readFileSync("./data.txt").toString("utf-8");
 const data = text.split(/\n{2,}/g);
+
 let mappedData = data.map((e) => e.replace(/\n/g, ""));
 
 let process = (data) => {
@@ -23,4 +24,43 @@ function removeDup(arr) {
   return Object.keys(obj);
 }
 
-console.log(process(mappedData));
+// console.log(process(mappedData));
+
+// Part 2
+
+let mappedData2 = data.map((e) => e.split(/\n/g));
+
+function findSameAns(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    count = count + comparison(arr[i]);
+  }
+  return count;
+}
+
+function comparison(arr) {
+  let shortInd = findShortest(arr);
+  let sameAnswer = 0;
+  for (let j = 0; j < arr[shortInd].length; j++) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].split("").some((e) => e === arr[shortInd][j])) {
+        count++;
+      }
+    }
+    if (count == arr.length) {
+      sameAnswer++;
+    }
+  }
+  return sameAnswer;
+}
+
+function findShortest(arr) {
+  let lengthsArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    lengthsArr.push(arr[i].length);
+  }
+  return lengthsArr.indexOf(Math.min(...lengthsArr));
+}
+
+console.log(findSameAns(mappedData2));
