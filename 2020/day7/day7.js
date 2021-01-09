44afbba492de0826c7e983f0c19acf37a17fb79a
allezz <aerbay@gmail.com>
2021-01-09 03:38:18 +0100

    Refactor

--- 2020/day7/day7.js
+++ 2020/day7/day7.js
@@ -2,36 +2,6 @@ const fs = require("fs");
 const text = fs.readFileSync("./data.txt").toString("utf-8");
 const data = text.split("\n");
 
-function checkBag(bag) {
-  let inside = bag.match(/\d\s\w+\s\w+/g);
-  if (inside !== null) {
-    inside.forEach((element) => {
-      if (element.split(" ")[1] == "shiny" && element.split(" ")[2] == "gold") {
-        return true;
-      }
-    });
-  }
-}
-
-function findBagInd(str1, str2) {
-  for (let i = 0; i < data.length; i++) {
-    if (str1 == data[i].split(" ")[0] && str2 == data[i].split(" ")[1]) {
-      return i;
-    }
-  }
-}
-
-function bagContent(bag) {
-  let inside = bag.match(/\d\s\w+\s\w+/g);
-  let bags = [];
-  if (inside !== null) {
-    inside.forEach((element) => {
-      bags.push(element.split(" ")[1] + " " + element.split(" ")[2]);
-    });
-  }
-  return bags;
-}
-
 /////////////////////////
 let bigList = [];
 function findGold(arr) {
