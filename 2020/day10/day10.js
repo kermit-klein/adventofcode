a2c83273a79136cced0c5d60a585f456bdfe9eef
allezz <aerbay@gmail.com>
2021-01-17 00:35:05 +0100

    Add day10 quick sort

--- 2020/day10/day10.js
+++ 2020/day10/day10.js
@@ -1,4 +1,22 @@
 const fs = require("fs");
 const text = fs.readFileSync("./data.txt").toString("utf-8");
-const data = text.replace(/^\s*$[\n\r]*/, "1");
-console.log(data);
+const data = text.split("\n").map((e) => parseInt(e));
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let left = [];
+  let right = [];
+  let pivot = arr.pop();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] <= pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort(data));
