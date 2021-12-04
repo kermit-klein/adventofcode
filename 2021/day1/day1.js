0f1f5ae3b8a93c69f28d6493b7d576bcad172ced
allezz <aerbay@gmail.com>
2021-12-04 20:33:41 +0100

    Add day1 second part solution

--- 2021/day1/day1.js
+++ 2021/day1/day1.js
@@ -2,14 +2,26 @@ const fs = require("fs");
 const text = fs.readFileSync("./data.txt").toString("utf-8");
 const data = text.split("\n");
 
-let count = 0;
+let sonarSweep = (sweep) => {
+  let count = 0;
+  sweep.forEach((element, index) => {
+    if (
+      parseInt(element) < parseInt(sweep[index + 1]) &&
+      index !== sweep.length - 1
+    ) {
+      count++;
+    }
+  });
+  console.log(count);
+};
+
+sonarSweep(data);
+let threeMeasure = [];
 data.forEach((element, index) => {
-  if (
-    parseInt(element) < parseInt(data[index + 1]) &&
-    index !== data.length - 1
-  ) {
-    count++;
+  if (data[index + 2]) {
+    threeMeasure.push(
+      parseInt(element) + parseInt(data[index + 1]) + parseInt(data[index + 2])
+    );
   }
 });
-
-console.log(count);
+sonarSweep(threeMeasure);
