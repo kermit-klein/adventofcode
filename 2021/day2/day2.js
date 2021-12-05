0a35fce3a8fc7ff76e046794522c4bb7be897157
allezz <aerbay@gmail.com>
2021-12-05 18:04:00 +0100

    Add day2 solution part2

--- 2021/day2/day2.js
+++ 2021/day2/day2.js
@@ -21,3 +21,23 @@ let findPos = (data) => {
 };
 
 findPos(data);
+
+let findPos2 = (data) => {
+  let verticalPos = 0;
+  let horizontalPos = 0;
+  let aim = 0;
+  data.forEach((element) => {
+    let splittedEle = element.split(" ");
+    if (splittedEle[0] === "forward") {
+      horizontalPos += parseInt(splittedEle[1]);
+      verticalPos += aim * parseInt(splittedEle[1]);
+    } else if (splittedEle[0] === "up") {
+      aim -= parseInt(splittedEle[1]);
+    } else {
+      aim += parseInt(splittedEle[1]);
+    }
+  });
+  console.log(verticalPos, horizontalPos, verticalPos * horizontalPos);
+};
+
+findPos2(data);
