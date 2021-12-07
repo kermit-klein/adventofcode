fcf8932e3e1f0958607c76ddddb9093c8f0dd745
allezz <aerbay@gmail.com>
2021-12-07 21:13:30 +0100

    Add day3 part2 solution

--- 2021/day3/day3.js
+++ 2021/day3/day3.js
@@ -22,11 +22,40 @@ let findGamma = (data) => {
   return gamma;
 };
 
-findGamma(data);
+// findGamma(data);
 
 let findEpsilon = (gamma) => {
   let epsilon = gamma.split("").map((e) => (e === "0" ? "1" : "0"));
   console.log(parseInt(epsilon.join(""), 2));
 };
 
-findEpsilon(findGamma(data));
+// findEpsilon(findGamma(data));
+
+let oxygenGenRating = (data) => {
+  let intermediateData = [...data];
+  let i = 0;
+  while (intermediateData.length > 1) {
+    let count1 = 0;
+    let count0 = 0;
+    for (let j = 0; j < intermediateData.length; j++) {
+      if (intermediateData[j][i] === "0") {
+        count0++;
+      } else {
+        count1++;
+      }
+    }
+    if (count1 >= count0) {
+      intermediateData = intermediateData.filter(
+        (element) => element[i] === "0"
+      );
+    } else {
+      intermediateData = intermediateData.filter(
+        (element) => element[i] === "1"
+      );
+    }
+    i++;
+  }
+  console.log(parseInt(intermediateData, 2));
+};
+
+oxygenGenRating(data);
